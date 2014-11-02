1598e882675e249600e467ef1f6e8e6e7a768b6a
skyliou520 <skyliou520@gmail.com>
2014-11-02 15:12:09 +0800

    1. call segue by manual.....(use performSegueWithIdentifier)

--- Adv1_4/ViewController.m
+++ Adv1_4/ViewController.m
@@ -5,7 +5,8 @@
 //  Created by pw's air on 2014/11/2.
 //  Copyright (c) 2014年 pw's air. All rights reserved.
 //
-
+//  set up segue by manual.
+//
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -13,9 +14,13 @@
 @end
 
 @implementation ViewController
-
+- (void) displayBlue
+{
+    [self performSegueWithIdentifier:@"bule" sender:nil];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(displayBlue) userInfo:nil repeats:NO];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
